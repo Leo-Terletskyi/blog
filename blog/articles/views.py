233eadd601286715827a9f0002d7ed795fe6ffe7
Leonid Terletskyi <leo.terletskyi@gmail.com>
2023-03-22 14:27:39 +0200

    added home page template, view

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.views import generic
 
@@ -8,3 +9,9 @@ class HomePage(generic.ListView):
     model = Article
     context_object_name = 'articles'
     template_name = 'articles/home_page.html'
+    queryset = Article.objects.all().select_related('category', 'author')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['categories'] = Category.objects.all().annotate(count_articles=Count('article'))
+        return context
--- blog/config/settings.py
+++ blog/config/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'articles',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Debug toolbar
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
